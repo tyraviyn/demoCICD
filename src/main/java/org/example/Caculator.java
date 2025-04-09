f28dd94800539c3ab3a5a72aa877195b1d7efb24
tyraviyn <thaovyt55@gmail.com>
2025-04-09 09:55:40 +0700

    test pass

--- src/main/java/org/example/Caculator.java
+++ src/main/java/org/example/Caculator.java
@@ -3,7 +3,7 @@ package org.example;
 public class Caculator {
 
     public Integer sum(Integer a, Integer b) {
-        return a/b;
+        return a+b;
     }
 
     public Integer sub(Integer a, Integer b) {
